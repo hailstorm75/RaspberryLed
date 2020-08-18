7d281cb935d72eb9c76abb7b33962d7c3c4a514b
Denis Akopyan <denis.akopyan@seznam.cz>
2020-08-18 23:09:27 +0200

    Upload solution

---
+++ src/LEDServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raspberry.IO.GeneralPurpose;
+using Raspberry.IO.GeneralPurpose.Behaviors;
+
+namespace LEDServer
+{
+  public static class Program
+  {
+    public static void Main(string[] args)
+    {
+      var driver = new GpioConnectionDriver();
+    }
+  }
+}
